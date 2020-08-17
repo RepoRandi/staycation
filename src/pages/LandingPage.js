35036dfa4e2c4712da6cfab17787c4934b215285
Randi Maulana Akbar <randimaulanaakbar@gmail.com>
2020-08-17 11:42:37 +0700

    Create Categories Component and put to arrange LandingPage

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import Header from "parts/Header";
 import Hero from "parts/Hero";
 import MostPicked from "parts/MostPicked";
+import Categories from "parts/Categories";
 
 import landingPage from "json/landingPage.json";
 export default class LandingPage extends Component {
@@ -19,6 +20,7 @@ export default class LandingPage extends Component {
           refMostPicked={this.refMostPicked}
           data={landingPage.mostPicked}
         />
+        <Categories data={landingPage.categories} />
       </>
     );
   }
---
+++ src/parts/Categories.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+import Button from "elements/Button";
+
+export default function Categories({ data }) {
+  return data.map((category, index1) => {
+    return (
+      <section className="container" key={`category-${index1}`}>
+        <h4 className="mb-3 font-weight-medium">{category.name}</h4>
+        <div className="container-grid">
+          {category.items.length === 0 ? (
+            <div className="row">
+              <div className="col-auto align-items-center">
+                There is no property at this category
+              </div>
+            </div>
+          ) : (
+            category.items.map((item, index2) => {
+              return (
+                <div
+                  className="item column-3 row-1"
+                  key={`category-${index1}-item-${index2}`}
+                >
+                  <div className="card">
+                    {item.isPopular && (
+                      <div className="tag">
+                        Popular{" "}
+                        <span className="font-weight-light">Choice</span>
+                      </div>
+                    )}
+                    <figure className="img-wrapper" style={{ height: 180 }}>
+                      <img
+                        src={item.imageUrl}
+                        alt={item.name}
+                        className="img-cover"
+                      />
+                    </figure>
+                    <div className="meta-wrapper">
+                      <Button
+                        type="link"
+                        className="stretched-link d-block text-gray-800"
+                        href={`/properties/${item._id}`}
+                      >
+                        <h5 className="h4">{item.name}</h5>
+                      </Button>
+                      <span className="text-gray-500">
+                        {item.city}, {item.country}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              );
+            })
+          )}
+        </div>
+      </section>
+    );
+  });
+}
