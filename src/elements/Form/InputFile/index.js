e7f511ba9eb4951bfc38aa88fb18bf494bf65351
Randi Maulana Akbar <randimaulanaakbar@gmail.com>
2020-08-19 22:09:59 +0700

    Finis create componet Form/InputText & InputFile

---
+++ src/elements/Form/InputFile/index.js
@@ -0,0 +1,77 @@
+import React, { useState, useRef } from "react";
+
+import propTypes from "prop-types";
+
+import "./index.scss";
+
+export default function File(props) {
+  const {
+    value,
+    placeholder,
+    name,
+    accept,
+    prepend,
+    append,
+    outerClassName,
+    inputClassName,
+  } = props;
+
+  const refInputFile = useRef(null);
+
+  const onChange = (event) => {
+    const target = {
+      target: {
+        name: name,
+        value: event.target.value,
+      },
+    };
+  };
+
+  return (
+    <div className={["input-text mb-3", outerClassName].join(" ")}>
+      <div className="input-group">
+        {prepend && (
+          <div className="input-group-prepend bg-gray-900">
+            <span className="input-group-text">{prepend}</span>
+          </div>
+        )}
+        <input
+          accept={accept}
+          ref={refInputFile}
+          name={name}
+          className="d-none"
+          type="file"
+          value={value}
+          onChange={onChange}
+        />
+        <input
+          onClick={() => refInputFile.current.click()}
+          defaultValue={value}
+          placeholder={placeholder}
+          className={["form-control", inputClassName].join(" ")}
+        />
+        {append && (
+          <div className="input-group-append bg-gray-900">
+            <span className="input-group-text">{append}</span>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+Text.defaultProps = {
+  placeholder: "Browse a file...",
+};
+
+Text.propTypes = {
+  name: propTypes.string.isRequired,
+  accept: propTypes.string.isRequired,
+  value: propTypes.string.isRequired,
+  onChange: propTypes.func.isRequired,
+  prepend: propTypes.oneOfType([propTypes.number, propTypes.string]),
+  append: propTypes.oneOfType([propTypes.number, propTypes.string]),
+  placeholder: propTypes.string,
+  outerClassName: propTypes.string,
+  inputClassName: propTypes.string,
+};
