4e7989fe7c5df4a124075b78df7c4cb7544301d6
Randi Maulana Akbar <randimaulanaakbar@gmail.com>
2020-08-19 16:50:12 +0700

    Improve parts component Header & Create page Checkout

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import LandingPage from "pages/LandingPage";
 import DetailsPage from "pages/DetailsPage";
+import Checkout from "pages/Checkout";
 import Example from "pages/Example";
 
 import "assets/scss/style.scss";
@@ -13,6 +14,7 @@ function App() {
       <Router>
         <Route exact path="/" component={LandingPage} />
         <Route exact path="/properties/:id" component={DetailsPage} />
+        <Route path="/checkout" component={Checkout} />
         <Route path="/example" component={Example} />
       </Router>
     </div>
--- src/parts/Header.js
+++ src/parts/Header.js
@@ -9,6 +9,21 @@ export default function Header(props) {
     return props.location.pathname === path ? " active" : "";
   };
 
+  if (props.isCentered)
+    return (
+      <Fade>
+        <header className="spacing-sm">
+          <div className="container">
+            <nav className="navbar navbar-expand-lg navbar-light">
+              <Button className="brand-text-icon mx-auto" href="" type="link">
+                Stay<span className="text-gray-900">cation.</span>
+              </Button>
+            </nav>
+          </div>
+        </header>
+      </Fade>
+    );
+
   return (
     <Fade>
       <header className="spacing-sm">
