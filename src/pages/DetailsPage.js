8ed14c3ce06b434540cfbeaff90cceeca5615bd8
Randi Maulana Akbar <randimaulanaakbar@gmail.com>
2020-08-19 15:09:20 +0700

    Put part component Categories, Testimony & Finish develop Page Details

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -5,6 +5,9 @@ import PageDetailTitle from "parts/PageDetailTitle";
 import FeaturedImage from "parts/FeaturedImage";
 import PageDetailDescription from "parts/PageDetailDescription";
 import BookingForm from "parts/BookingForm";
+import Categories from "parts/Categories";
+import Testimony from "parts/Testimony";
+import Footer from "parts/Footer";
 
 import ItemDetails from "json/itemDetails.json";
 
@@ -22,22 +25,22 @@ export default class DetailsPage extends Component {
 
     return (
       <>
-        <Header {...this.props}></Header>
-        <PageDetailTitle
-          breadcrumb={breadcrumb}
-          data={ItemDetails}
-        ></PageDetailTitle>
-        <FeaturedImage data={ItemDetails.imageUrls}></FeaturedImage>
+        <Header {...this.props} />
+        <PageDetailTitle breadcrumb={breadcrumb} data={ItemDetails} />
+        <FeaturedImage data={ItemDetails.imageUrls} />
         <section className="container">
           <div className="row">
             <div className="col-7 pr-5">
-              <PageDetailDescription data={ItemDetails}></PageDetailDescription>
+              <PageDetailDescription data={ItemDetails} />
             </div>
             <div className="col-5">
               <BookingForm itemDetails={ItemDetails} />
             </div>
           </div>
         </section>
+        <Categories data={ItemDetails.categories} />
+        <Testimony data={ItemDetails.testimonial} />
+        <Footer />
       </>
     );
   }
--- src/parts/BookingForm.js
+++ src/parts/BookingForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Fade from "react-reveal/Fade";
 
 import propTypes from "prop-types";
 
@@ -70,52 +71,54 @@ export default class BookingForm extends Component {
     const { itemDetails, startBooking } = this.props;
 
     return (
-      <div className="card bordered" style={{ padding: "60px 80px" }}>
-        <h4 className="mb-3">Start Booking</h4>
-        <h5 className="h2 text-teal mb-4">
-          ${itemDetails.price}{" "}
-          <span className="text-gray-500 font-weight-light">
-            per {itemDetails.unit}
-          </span>
-        </h5>
+      <Fade bottom>
+        <div className="card bordered" style={{ padding: "60px 80px" }}>
+          <h4 className="mb-3">Start Booking</h4>
+          <h5 className="h2 text-teal mb-4">
+            ${itemDetails.price}{" "}
+            <span className="text-gray-500 font-weight-light">
+              per {itemDetails.unit}
+            </span>
+          </h5>
 
-        <label htmlFor="duration">How long you will stay?</label>
-        <InputNumber
-          max={30}
-          suffix={" night"}
-          isSuffixPlural
-          onChange={this.updateData}
-          name="duration"
-          value={data.duration}
-        />
+          <label htmlFor="duration">How long you will stay?</label>
+          <InputNumber
+            max={30}
+            suffix={" night"}
+            isSuffixPlural
+            onChange={this.updateData}
+            name="duration"
+            value={data.duration}
+          />
 
-        <label htmlFor="date">Pick a date</label>
-        <InputDate onChange={this.updateData} name="date" value={data.date} />
+          <label htmlFor="date">Pick a date</label>
+          <InputDate onChange={this.updateData} name="date" value={data.date} />
 
-        <h6
-          className="text-gray-500 font-weight-light"
-          style={{ marginBottom: 40 }}
-        >
-          You will pay{" "}
-          <span className="text-gray-900">
-            ${itemDetails.price * data.duration} USD
-          </span>{" "}
-          per{" "}
-          <span className="text-gray-900">
-            {data.duration} {itemDetails.unit}
-          </span>
-        </h6>
+          <h6
+            className="text-gray-500 font-weight-light"
+            style={{ marginBottom: 40 }}
+          >
+            You will pay{" "}
+            <span className="text-gray-900">
+              ${itemDetails.price * data.duration} USD
+            </span>{" "}
+            per{" "}
+            <span className="text-gray-900">
+              {data.duration} {itemDetails.unit}
+            </span>
+          </h6>
 
-        <Button
-          className="btn"
-          hasShadow
-          isPrimary
-          isBlock
-          onClick={startBooking}
-        >
-          Continue to book
-        </Button>
-      </div>
+          <Button
+            className="btn"
+            hasShadow
+            isPrimary
+            isBlock
+            onClick={startBooking}
+          >
+            Continue to book
+          </Button>
+        </div>
+      </Fade>
     );
   }
 }
